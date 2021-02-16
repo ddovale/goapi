5090b25820652a360762a2de283a866f1ee3f3fb
ddovale <dovale.daniel07@gmail.com>
2021-02-15 21:37:50 -0300

    Books API

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/ddovale/goapi/controllers"
+	"github.com/ddovale/goapi/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+	})
+
+	models.ConnectDataBase()
+
+	r.GET("/books", controllers.FindBooks)
+	r.POST("/books", controllers.CreateBook)
+	r.GET("/books/:id", controllers.FindBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id")
+
+	r.Run()
+}
